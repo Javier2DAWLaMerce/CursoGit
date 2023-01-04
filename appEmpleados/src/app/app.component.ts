d3e1a3e309d1096be97cb822cfaa545f56f91100
Javier <jorduna.daw2n22@lamerce.com>
2023-01-04 21:27:19 +0100

    Añadimos clase empleado y empezamos a construir el html y css de la app

--- appEmpleados/src/app/app.component.ts
+++ appEmpleados/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   titulo = 'Listado de Empleados';
+  empleados : Empleado[]=[
+
+    new Empleado("Javier","Orduña","Presidente",100000),
+    new Empleado("Elisabet","Farrés","Presidenta",100000),
+    new Empleado("Eula","Orduña","Comilona",80000),
+    new Empleado("Noelle","Farrés","Dormilona",80000),
+  ]
 }
