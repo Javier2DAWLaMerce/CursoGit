78c1884a9fdbd6de861027890c4a8e299c693e5d
Javier <jorduna.daw2n22@lamerce.com>
2023-01-05 11:00:25 +0100

    Añadida funcionalidad al formulario

--- appEmpleados/src/app/app.component.ts
+++ appEmpleados/src/app/app.component.ts
@@ -8,11 +8,23 @@ import { Empleado } from './empleado.model';
 })
 export class AppComponent {
   titulo = 'Listado de Empleados';
-  empleados : Empleado[]=[
+  empleados: Empleado[] = [
 
-    new Empleado("Javier","Orduña","Presidente",100000),
-    new Empleado("Elisabet","Farrés","Presidenta",100000),
-    new Empleado("Eula","Orduña","Comilona",80000),
-    new Empleado("Noelle","Farrés","Dormilona",80000),
-  ]
+    new Empleado("Javier", "Orduña", "Presidente", 100000),
+    new Empleado("Elisabet", "Farrés", "Presidenta", 100000),
+    new Empleado("Eula", "Orduña", "Comilona", 80000),
+    new Empleado("Noelle", "Farrés", "Dormilona", 80000),
+  ];
+  //Recogemos del formulario los datos que el usuario introduzca
+  formularioNombre: string = "";
+  formularioApellido: string = "";
+  formularioCargo: string = "";
+  formularioSalario: number = 0;
+
+  agregarEmpleado() {//Con la función que invoca el botón del formulario introducimos los datos en un nuevo empleado
+    let nuevoEmpleado = new Empleado(this.formularioNombre, this.formularioApellido, this.formularioCargo, this.formularioSalario);
+    //Agregamos al objeto empleados un nuevo empleado con la función push()
+    this.empleados.push(nuevoEmpleado)
+  }
+
 }
